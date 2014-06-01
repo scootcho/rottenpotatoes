ff3b54c229fd94463212c6d570fd20b0b602d968
Scott Yu <scott.tj.yu@gmail.com>
2014-06-01 11:04:21 -0300

    hw 2 part 1 complete

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,13 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+
+  def helper_class(field)
+  	if (params[:sort].to_s == field)
+  	  return "hilite"
+  	else
+  	  return nil
+  	end
+  end
 end
+
